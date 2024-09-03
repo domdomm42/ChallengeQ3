0608eb8f1b09d5f0fcc00842327dc9b0064ce179
dom <limoudom2001@gmail.com>
2024-09-03 12:11:42 +1000

    Clean up spacing

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,13 @@ import issuesRoute from "./routes/issuesRoute.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Home page");
 });
 
 app.use("/issues", issuesRoute);
