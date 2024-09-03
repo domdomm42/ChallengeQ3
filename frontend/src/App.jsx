9e3d6bd802b0875a4e1c1260aceaeebe934a1c90
dom <limoudom2001@gmail.com>
2024-09-03 11:24:45 +1000

    feat: Created Edit Issue page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import CreateIssue from "./pages/CreateIssue";
 import ShowIssue from "./pages/ShowIssue";
+import EditIssue from "./pages/EditIssue";
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/issues/create" element={<CreateIssue />} />
       <Route path="/issues/details/:id" element={<ShowIssue />} />
+      <Route path="/issues/edit/:id" element={<EditIssue />} />
     </Routes>
   );
 };
